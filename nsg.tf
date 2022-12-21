4ddea0c5dfb4d6913efd81b4bed2e293bbce579c
Thomas Plisson <tplisson@paloaltonetworks.com>
2022-12-21 09:54:55 -0500

    2022-12-21

--- nsg.tf
+++ nsg.tf
@@ -46,4 +46,17 @@ resource "azurerm_network_security_rule" "ccs_az_nsg_rule3" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
   network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
-}
+} 
+resource "azurerm_network_security_rule" "ccs_az_nsg_rule4" {
+  name                        = "ccsaznsgrule4"
+  priority                    = 104
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
+  network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
+} 
