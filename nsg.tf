f54805987f2aa6452c46c89f96d498bd9d8d85fc
Thomas Plisson <tplisson@paloaltonetworks.com>
2023-01-04 11:09:22 -0500

    2023-01-04

--- nsg.tf
+++ nsg.tf
@@ -34,19 +34,19 @@ resource "azurerm_network_security_rule" "ccs_az_nsg_rule2" {
   resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
   network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
 }
-resource "azurerm_network_security_rule" "ccs_az_nsg_rule3" {
-  name                        = "ccsaznsgrule3"
-  priority                    = 103
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Udp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
-  network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
-} 
+# resource "azurerm_network_security_rule" "ccs_az_nsg_rule3" {
+#   name                        = "ccsaznsgrule3"
+#   priority                    = 103
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Udp"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
+#   network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
+# } 
 resource "azurerm_network_security_rule" "ccs_az_nsg_rule4" {
   name                        = "ccsaznsgrule4"
   priority                    = 104
@@ -60,4 +60,3 @@ resource "azurerm_network_security_rule" "ccs_az_nsg_rule4" {
   resource_group_name         = azurerm_resource_group.ccs_az_nsg.name
   network_security_group_name = azurerm_network_security_group.ccs_az_nsg_nsg.name
 } 
-
